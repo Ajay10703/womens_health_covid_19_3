fddf7559aae2ed45fb694d1f03040b9d0dc6b389
AJAY KUMAR <ajay10703@gmail.com>
2023-04-05 16:18:54 +0530

    first commit

---
+++ js/modern-business.js
@@ -0,0 +1,13 @@
+// Activates the Carousel
+$('#myCarousel').carousel({
+  interval: 8000
+})
+$('#myCarousel2').carousel({
+  interval: 4000
+
+})
+
+// Activates Tooltips for Social Links
+$('.tooltip-social').tooltip({
+  selector: "a[data-toggle=tooltip]"
+})
